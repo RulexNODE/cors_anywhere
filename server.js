29a1429a3631b15b081a96ce6c06b13b601b5612
Michael McMillen <michael.mc09@gmail.com>
2021-02-19 11:48:45 +0000

    Added Express To Handle API

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = process.env.HOST || '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Express running on ${ port }`);
+});
+
+const geocodeAPIKey = '82845a74c5cd488caf782fd4074540b3';
+
+app.get('/geocodeAPI', function(req,res){
+    res.send(geocodeAPIKey);
+});
  
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
