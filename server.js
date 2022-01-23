d4fc7bc83a8d45fcf2f4e41ce00a65b56e0354e1
Michael McMillen <michael.mc09@gmail.com>
2022-01-23 12:40:37 +0000

    Updated Comments

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// cors-anywhere deployed as my own server due to the public demo URL
+// restricted to a certain number of uses
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
