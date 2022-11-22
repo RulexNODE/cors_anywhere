a8979bf3ee20cd14192def6087142d53bc70334a
RulexNODE <118261010+RulexNODE@users.noreply.github.com>
2022-11-22 18:27:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ var port = process.env.PORT || '8080';
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], //'https://michaelmcmillen.github.io'
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+  checkRateLimit: checkRateLimit,
+  removeHeaders: ['cookie', 'cookie2'],
+  redirectSameOrigin: true,
+  httpProxyOptions: {xfwd: false},
 }).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
