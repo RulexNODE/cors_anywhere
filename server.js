ae9d7d7ad1c940f275531520171adb1c918971e5
RulexNODE <118261010+RulexNODE@users.noreply.github.com>
2022-11-30 10:00:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var port = process.env.PORT || '8080';
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    removeHeaders: ['cookie', 'cookie2'],
-  redirectSameOrigin: true
+  removeHeaders: ['cookie', 'cookie2'],
+  redirectSameOrigin: true,
+  httpProxyOptions: {xfwd: false},
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
